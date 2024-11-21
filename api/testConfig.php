a09152664fc06075c8467ff374b541dc915ec176
Lucas Tamashiro <114181346+lucastamashirolyt@users.noreply.github.com>
2024-11-21 12:03:01 -0300

    Delete api directory

--- api/testConfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once('config.php');
-
-if (isset($conn)) {
-    echo "Conexão com o banco de dados estabelecida com sucesso!";
-} else {
-    echo "Falha na conexão com o banco de dados.";
-}
-?>
